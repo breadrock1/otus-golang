36bb6fa39b577e8815aeb94c381edacad4993a70
breadrock <breadrock1@gmail.com>
2024-01-03 13:18:10 +0300

    chore(unpack_test): added additional test cases

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,19 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		//{input: `qwe\4\5`, expected: `qwe45`},
+		//{input: `qwe\45`, expected: `qwe44444`},
+		//{input: `qwe\\5`, expected: `qwe\\\\\`},
+		//{input: `qwe\\\3`, expected: `qwe\3`},
+
+		// own test cases
+		{input: "a3a2b0c", expected: "aaaaac"},
+		{input: "a0b0c0", expected: ""},
+		{input: "a b1", expected: "ab"},
+		{input: " 3", expected: ""},
+		{input: "a3  ", expected: "aaa"},
 	}
 
 	for _, tc := range tests {
