55ef1754edc45fbd3654128b00bc7ab6ea984f57
breadrock <breadrock1@gmail.com>
2024-01-03 13:17:45 +0300

    chore(unpack): impled Unpack method into unpack package

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,7 +9,50 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputString string) (string, error) {
+	// Check if input sentence length is empty -> returns input string back.
+	if len(inputString) < 1 {
+		return inputString, nil
+	}
+
+	runeArray := []rune(inputString)
+	var prevSymbol = runeArray[0]
+	// Check first symbol of input sentence -> mustn't be a digit.
+	if unicode.IsDigit(prevSymbol) {
+		return "", ErrInvalidString
+	}
+
+	var deserializedString strings.Builder
+	for _, currSymbol := range runeArray[1:] {
+
+		if unicode.IsDigit(currSymbol) {
+			// Catches case when near symbols are digits => bigger then 9.
+			if unicode.IsDigit(prevSymbol) {
+				return "", ErrInvalidString
+			}
+
+			if unicode.IsLetter(prevSymbol) {
+				str := string(currSymbol)
+				number, _ := strconv.Atoi(str)
+				str = strings.Repeat(string(prevSymbol), number)
+				deserializedString.WriteString(str)
+			}
+
+		} else {
+			if unicode.IsLetter(prevSymbol) {
+				str := string(prevSymbol)
+				deserializedString.WriteString(str)
+			}
+		}
+
+		prevSymbol = currSymbol
+	}
+
+	// Processing last sentence symbol.
+	str := string(prevSymbol)
+	if unicode.IsLetter(prevSymbol) {
+		deserializedString.WriteString(str)
+	}
+
+	return deserializedString.String(), nil
 }
