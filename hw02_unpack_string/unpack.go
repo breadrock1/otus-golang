d8ce0ef7633256465663abdc0393da9c8c8cb8d4
breadrock <breadrock1@gmail.com>
2024-01-04 22:47:20 +0300

    chore(unpack): re-impled unpack algorithm

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,43 +12,72 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(inputString string) (string, error) {
 	// Check if input sentence length is empty -> returns input string back.
 	if len(inputString) < 1 {
-		return inputString, nil
+		return "", nil
 	}
 
+	var deserializedSlice []string
 	runeArray := []rune(inputString)
-	prevSymbol := runeArray[0]
-
-	// Check first symbol of input sentence -> mustn't be a digit.
-	if unicode.IsDigit(prevSymbol) {
-		return "", ErrInvalidString
-	}
-
-	var deserializedString strings.Builder
-	for _, currSymbol := range runeArray[1:] {
-		if unicode.IsDigit(currSymbol) {
-			// Catches case when near symbols are digits => bigger then 9.
-			if unicode.IsDigit(prevSymbol) {
-				return "", ErrInvalidString
-			}
-			if unicode.IsLetter(prevSymbol) {
-				str := string(currSymbol)
-				number, _ := strconv.Atoi(str)
-				str = strings.Repeat(string(prevSymbol), number)
-				deserializedString.WriteString(str)
-			}
-		} else if unicode.IsLetter(prevSymbol) {
-			str := string(prevSymbol)
-			deserializedString.WriteString(str)
+	for offset := len(runeArray) - 1; offset >= 0; offset-- {
+		rCurrSymbol := runeArray[offset]
+		if offset == 0 {
+			deserializedSlice = append(deserializedSlice, string(rCurrSymbol))
+			continue
 		}
 
-		prevSymbol = currSymbol
+		rPrevSymbol := runeArray[offset-1]
+		if unicode.IsSymbol(rCurrSymbol) || unicode.IsLetter(rCurrSymbol) {
+			deserializedSlice = append(deserializedSlice, string(rCurrSymbol))
+			continue
+		}
+
+		if unicode.IsDigit(rCurrSymbol) {
+			if !isArabicDigit(rCurrSymbol) {
+				deserializedSlice = append(deserializedSlice, string(rCurrSymbol))
+				continue
+			}
+
+			if unicode.IsDigit(rPrevSymbol) && isArabicDigit(rPrevSymbol) {
+				return "", ErrInvalidString
+			}
+
+			repeated := extractSymbolData(rPrevSymbol, rCurrSymbol)
+			deserializedSlice = append(deserializedSlice, repeated)
+
+			//if unicode.IsDigit(rPrevSymbol) {
+			//	if isArabicDigit(rPrevSymbol) {
+			//		return "", ErrInvalidString
+			//	}
+			//	repeated := extractSymbolData(rPrevSymbol, rCurrSymbol)
+			//	deserializedSlice = append(deserializedSlice, repeated)
+			//} else if unicode.IsSymbol(rPrevSymbol) || unicode.IsLetter(rPrevSymbol) {
+			//	repeated := extractSymbolData(rPrevSymbol, rCurrSymbol)
+			//	deserializedSlice = append(deserializedSlice, repeated)
+			//}
+
+			offset--
+		}
 	}
 
-	// Processing last sentence symbol.
-	str := string(prevSymbol)
-	if unicode.IsLetter(prevSymbol) {
-		deserializedString.WriteString(str)
-	}
+	resultString := revertDeserializedData(deserializedSlice)
+	return resultString, nil
+}
 
-	return deserializedString.String(), nil
+func revertDeserializedData(sliceData []string) string {
+	var reversedSlice = make([]string, len(sliceData))
+	for offset := len(sliceData) - 1; offset >= 0; offset-- {
+		currSymbol := sliceData[offset]
+		newOffset := len(sliceData) - 1 - offset
+		reversedSlice[newOffset] = currSymbol
+	}
+	return strings.Join(reversedSlice, "")
+}
+
+func isArabicDigit(runeData rune) bool {
+	_, isError := strconv.Atoi(string(runeData))
+	return isError == nil
+}
+
+func extractSymbolData(runeSymbol rune, decodeSymbol rune) string {
+	number, _ := strconv.Atoi(string(decodeSymbol))
+	return strings.Repeat(string(runeSymbol), number)
 }
