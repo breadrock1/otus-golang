1064434c08a185f55d13d93088cd0c6d61cfdcfd
breadrock <breadrock1@gmail.com>
2024-01-05 02:42:41 +0300

    fix(unpack): fix linter warnings

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -55,7 +55,7 @@ func Unpack(inputString string) (string, error) {
 }
 
 func revertDeserializedData(sliceData []string) string {
-	var reversedSlice = make([]string, len(sliceData))
+	var reversedSlice []string
 	for offset := len(sliceData) - 1; offset >= 0; offset-- {
 		currSymbol := sliceData[offset]
 		newOffset := len(sliceData) - 1 - offset
