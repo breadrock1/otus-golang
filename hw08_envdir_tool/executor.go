417673d7165e8cb5e808ecb8d80e6ada221d0ddc
breadrock <breadrock1@gmail.com>
2024-01-07 18:24:52 +0300

    chore(executor): impled executor.go package with main functionality

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Env = extractEnvironment(env)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			return exitError.ExitCode()
+		}
+	}
+
+	return 0
+}
+
+func extractEnvironment(env Environment) []string {
+	environVars := make([]string, 0)
+	for key, envVar := range env {
+		if envVar.NeedRemove {
+			continue
+		}
+
+		envValue := fmt.Sprintf("%s=%s", key, envVar.Value)
+		environVars = append(environVars, envValue)
+	}
+	return environVars
 }
