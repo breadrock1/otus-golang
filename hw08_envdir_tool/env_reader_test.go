bee7ea8829cf12155cb794be76c2fdb9f57235d2
breadrock <breadrock1@gmail.com>
2024-01-07 18:24:34 +0300

    chore(env_reader): impled tests for env_reader.go package

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,39 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("Success with testdata", func(t *testing.T) {
+		expectEnv := Environment{
+			"BAR":   EnvValue{Value: "bar"},
+			"FOO":   EnvValue{Value: "   foo\nwith new line"},
+			"HELLO": EnvValue{Value: `"hello"`},
+			"UNSET": EnvValue{NeedRemove: true},
+			"EMPTY": EnvValue{NeedRemove: true},
+		}
+		env, err := ReadDir("testdata/env")
+		require.NoError(t, err)
+		require.Equal(t, env, expectEnv)
+	})
+
+	t.Run("Success with empty dir", func(t *testing.T) {
+		dir, err := ioutil.TempDir("", "test")
+		require.NoError(t, err)
+		defer os.RemoveAll(dir)
+
+		env, err := ReadDir(dir)
+		require.NoError(t, err)
+		require.Len(t, env, 0)
+	})
+
+	t.Run("Fail with dir not exists", func(t *testing.T) {
+		_, err := ReadDir("some name")
+		require.Error(t, err)
+	})
 }
