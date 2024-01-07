ea66adfaa6fd76ed0da7fdac27d67435065e424c
breadrock <breadrock1@gmail.com>
2024-01-07 19:28:30 +0300

    chore(main): impled main.go file

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args[1:]
+	if len(args) < 2 {
+		printFatal("go-envdir: usage: go-envdir dir child")
+	}
+	env, err := ReadDir(args[0])
+	if err != nil {
+		printFatal(fmt.Errorf("go-envdir: fatal: %w", err))
+	}
+	code := RunCmd(args[1:], env)
+	if code == 101 {
+		printFatal(fmt.Errorf("go-envdir: unable to run %s", args[1]))
+	}
+	os.Exit(code)
+}
+
+func printFatal(a interface{}) {
+	_, _ = fmt.Fprintln(os.Stderr, a)
+	os.Exit(101)
 }
