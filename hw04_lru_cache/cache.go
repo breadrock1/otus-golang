037b9e6e716b55d6f7197f314137ef864ceaaa7c
breadrock <breadrock1@gmail.com>
2024-01-08 15:46:04 +0300

    fix(git): restored hw04 from master state

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,9 +1,5 @@
 package hw04lrucache
 
-import (
-	"sync"
-)
-
 type Key string
 
 type Cache interface {
@@ -13,59 +9,11 @@ type Cache interface {
 }
 
 type lruCache struct {
-	sync.Mutex
+	Cache // Remove me after realization.
+
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
-}
-
-func (l *lruCache) Set(key Key, value interface{}) bool {
-	l.Lock()
-	defer l.Unlock()
-
-	item, ok := l.items[key]
-
-	if ok {
-		l.queue.MoveToFront(item)
-		item.Value = value
-		return true
-	}
-
-	if l.queue.Len() == l.capacity {
-		removable := l.queue.Back()
-		l.queue.Remove(removable)
-		delete(l.items, key)
-	}
-
-	item = l.queue.PushFront(value)
-	l.items[key] = item
-
-	return false
-}
-
-func (l *lruCache) Get(key Key) (interface{}, bool) {
-	l.Lock()
-	defer l.Unlock()
-
-	item, ok := l.items[key]
-	if !ok {
-		return nil, false
-	}
-
-	l.queue.MoveToFront(item)
-	return item.Value, true
-}
-
-func (l *lruCache) Clear() {
-	l.Lock()
-	defer l.Unlock()
-
-	for l.queue.Len() > 0 {
-		lastItem := l.queue.Back()
-		l.queue.Remove(lastItem)
-	}
-
-	l.items = make(map[Key]*ListItem)
 }
 
 func NewCache(capacity int) Cache {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,97 +17,10 @@ type ListItem struct {
 }
 
 type list struct {
-	length int
-	front  *ListItem
-	back   *ListItem
-}
-
-func (l *list) Len() int {
-	return l.length
-}
-
-func (l *list) Front() *ListItem {
-	return l.front
-}
-
-func (l *list) Back() *ListItem {
-	return l.back
-}
-
-// PushFront new element to begin of linked list.
-// ItemName and fields: front, back
-// new      | a,            b,      c,
-// *a, nil  | *b, nil/*new  *c, *a  nil, *b.
-func (l *list) PushFront(v interface{}) *ListItem {
-	newItem := &ListItem{Value: v}
-
-	if l.length < 1 {
-		l.back = newItem
-	} else {
-		newItem.Next = l.front
-		l.front.Prev = newItem
-	}
-
-	l.front = newItem
-	l.length++
-	return newItem
-}
-
-// PushBack new element to end of linked list.
-// ItemName and fields: front, back
-// a,       b,      c,            | new
-// *b, nil  *c, *a  nil/*new, *b  | nil, *c.
-func (l *list) PushBack(v interface{}) *ListItem {
-	newItem := &ListItem{Value: v}
-
-	if l.length < 1 {
-		l.back = newItem
-	} else {
-		newItem.Prev = l.back
-		l.back.Next = newItem
-	}
-
-	l.back = newItem
-	l.length++
-	return newItem
-}
-
-// Remove element from linked list
-// ItemName and fields: front, back
-// a,       b,         | c,      | d
-// *b, nil  *c/*d, *a  | *d, *b  | nil, *c/*b.
-func (l *list) Remove(i *ListItem) {
-	if i.Prev == nil {
-		l.front = i.Next
-	} else {
-		i.Prev.Next = i.Next
-	}
-
-	if i.Next == nil {
-		l.back = i.Prev
-	} else {
-		i.Next.Prev = i.Prev
-	}
-
-	l.length--
-}
-
-func (l *list) MoveToFront(i *ListItem) {
-	if i.Prev == nil {
-		return
-	}
-
-	l.Remove(i)
-
-	i.Prev = nil
-	i.Next = l.front
-
-	l.front.Prev = i
-	l.front = i
-
-	l.length++
+	List // Remove me after realization.
+	// Place your code here.
 }
 
 func NewList() List {
-	return &list{}
+	return new(list)
 }
