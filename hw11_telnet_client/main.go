60a2bc03c8d2a5a3a5f646de6db25e2c34463010
breadrock <breadrock1@gmail.com>
2024-01-15 01:03:16 +0300

    chore(main): save point of main.go file

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	wg := &sync.WaitGroup{}
+	ctx := context.Background()
+	ctx, _ = context.WithTimeout(ctx, time.Second)
+	wg.Add(1)
+	go dealLongWithCtx(wg, ctx)
+	wg.Wait()
+
+	l, err := net.Listen("tcp", "0.0.0.0:3302")
+	if err != nil {
+		log.Fatalf("Cannot listen: %v", err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Cannot accept: %v", err)
+		}
+
+		//go handleConnection(conn)
+	}
 }
