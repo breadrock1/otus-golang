72144ffcf3826b0040917aba93aade689bdb6687
breadrock <breadrock1@gmail.com>
2024-02-24 15:51:17 +0300

    feat(validator): added module from sources

---
+++ hw09_struct_validator/common.go
@@ -0,0 +1,22 @@
+package hw09structvalidator //nolint:golint,stylecheck
+import "reflect"
+
+// tag to define rules of validation.
+const validatorTag = "validate"
+
+// structure validation rules array.
+type structRules []fieldRules
+
+// validation rules for single field.
+type fieldRules interface {
+	fieldName() string
+	validate(errs ValidationErrors, value reflect.Value) ValidationErrors
+}
+
+// field type to validate.
+type validateKind int
+
+const (
+	validateRegular validateKind = iota
+	validateSlice
+)
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -34,18 +34,118 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	PrivateField struct {
+		Login    string
+		password string `validate:"in:123456,pass"`
+	}
+
+	Ints struct {
+		IntField   int   `validate:"min:1|max:100"`
+		Int8Field  int8  `validate:"min:1|max:100"`
+		Int16Field int16 `validate:"min:1|max:100"`
+		Int32Field int32 `validate:"min:1|max:100"`
+		Int64Field int64 `validate:"min:1|max:100"`
+	}
 )
+
+var user = User{
+	ID:     "012345678901234567890123456789123456",
+	Name:   "Somebody",
+	Age:    20,
+	Email:  "test@mail.ru",
+	Role:   "admin",
+	Phones: []string{"79270000000"},
+	meta:   []byte("{}"),
+}
+
+var app = App{
+	Version: "12345",
+}
+
+var token = Token{
+	Header:    []byte("12345"),
+	Payload:   []byte("12345"),
+	Signature: []byte("12345"),
+}
+
+var response = Response{
+	Code: 200,
+	Body: "content",
+}
+
+var privateField = PrivateField{
+	Login:    "somebody",
+	password: "dv740Z_I!hrU&aW11dWYbrQ$t$QHez1*r@x%`WBU",
+}
+
+var ints = Ints{
+	IntField:   42,
+	Int8Field:  42,
+	Int16Field: 42,
+	Int32Field: 42,
+	Int64Field: 42,
+}
+
+var emptyUser = User{}
+
+var wrongUser = User{
+	ID:     "012345678",
+	Age:    51,
+	Email:  "test.mail.ru",
+	Role:   "hacker",
+	Phones: []string{"03"},
+}
+
+var wrongResponse = Response{
+	Code: 418,
+}
+
+var wrongInts = Ints{
+	IntField:   0,
+	Int8Field:  0,
+	Int16Field: 0,
+	Int32Field: 0,
+	Int64Field: 100500,
+}
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
-		in          interface{}
-		expectedErr error
+		name string
+		in   interface{}
 	}{
 		{
-			// Place your code here.
+			name: "nil",
+			in:   nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			name: "User",
+			in:   user,
+		},
+		{
+			name: "&User",
+			in:   &user,
+		},
+		{
+			name: "App",
+			in:   app,
+		},
+		{
+			name: "Token",
+			in:   token,
+		},
+		{
+			name: "Response",
+			in:   response,
+		},
+		{
+			name: "PrivateField",
+			in:   privateField,
+		},
+		{
+			name: "Ints",
+			in:   ints,
+		},
 	}
 
 	for i, tt := range tests {
@@ -57,4 +157,24 @@ func TestValidate(t *testing.T) {
 			_ = tt
 		})
 	}
+}
+
+func TestValidateFail(t *testing.T) {
+}
+
+func TestErrIncorrectUse(t *testing.T) {
+}
+
+func BenchmarkValidateSuccess(b *testing.B) {
+}
+
+func BenchmarkValidateFail(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_ = Validate(emptyUser)
+			_ = Validate(wrongUser)
+			_ = Validate(wrongResponse)
+			_ = Validate(wrongInts)
+		}
+	})
 }
