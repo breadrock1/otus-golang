d8d09764334a755944b9477d06edc6ef9990323f
Bread White <breadrock1@gmail.com>
2024-05-27 08:09:46 +0300

    chore(rabbit)!: save point for rabbit

---
+++ hw12_13_14_15_calendar/cmd/calendar_scheduler/calendar_scheduler.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/cmd"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/internal/logger"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/internal/rabbit"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/internal/storage"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/internal/storage/event"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var configFile string
+
+const (
+	removeTimeout = time.Minute * 5
+	checkTimout   = time.Minute
+)
+
+func newMessage(ev event.Event) rabbit.Message {
+	return rabbit.Message{
+		ID:     ev.ID,
+		Name:   ev.Title,
+		Time:   ev.Start,
+		UserID: ev.UserID,
+	}
+}
+
+func init() {
+	flag.StringVar(&configFile, "config", "./configs/config.toml", "Path to configuration file")
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.WarnLevel)
+}
+
+func main() {
+	config := cmd.Execute()
+
+	_, err := logger.New(&config.Logger)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rabbitService := rabbit.New(config.Rabbit)
+	if err = rabbitService.Connect(); err != nil {
+		log.Fatalln(err)
+	}
+	defer rabbitService.Close()
+
+	storageService := storage.New(&config.Database)
+
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+		if err := storageService.Close(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	startTime := time.Now().Add(-time.Minute)
+	endTime := time.Now()
+	checkTicker := time.NewTicker(checkTimout)
+	removeTicker := time.NewTicker(removeTimeout)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			log.Debugf("get events: %s - %s", startTime, endTime)
+			events, err := storageService.GetEventsByNotifier(ctx, startTime, endTime)
+			if err != nil {
+				log.Errorf("failed to get events: %s", err)
+				continue
+			}
+
+			for _, ev := range events {
+				log.Debugf("send event: %v", ev)
+				m := newMessage(ev)
+				data, _ := json.Marshal(m)
+				_ = rabbitService.Publish(data)
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case <-checkTicker.C:
+				log.Debug("ticker")
+				startTime = endTime
+				endTime = time.Now()
+			case <-removeTicker.C:
+				storageService.RemoveAfter(ctx, time.Now().Add(-1*(time.Hour*24*365)))
+			}
+		}
+	}
+}
---
+++ hw12_13_14_15_calendar/cmd/calendar_sender/calendar_sender.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/cmd"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/internal/logger"
+	"github.com/breadrock1/otus-golang/hw12_13_14_15_calendar/internal/rabbit"
+	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "./configs/config.toml", "Path to configuration file")
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.WarnLevel)
+}
+
+func main() {
+	config := cmd.Execute()
+
+	_, err := logger.New(&config.Logger)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rabbitService := rabbit.New(config.Rabbit)
+	if err = rabbitService.Connect(); err != nil {
+		log.Fatalln(err)
+	}
+	defer rabbitService.Close()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	_ = rabbitService.Consume(ctx, func(msg amqp.Delivery) {
+		m := rabbit.Message{}
+		err := json.Unmarshal(msg.Body, &m)
+		if err != nil {
+			log.Errorf("failed to parse bytes: %s", err)
+			cancel()
+			return
+		}
+		log.Printf("sending message %v", m)
+	})
+}
